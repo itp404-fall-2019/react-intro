63e23e4c6e7297961dd446cbbefbf7ef191ddb54
David Tang <david.tang@verizondigitalmedia.com>
2019-09-17 19:48:10 -0700

    adds search bar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends React.Component {
     this.state = {
       repos: [],
       members: [],
-      loading: false
+      loading: false,
+      searchValue: ''
     };
 
     this.handleClickReposButton = this.fetchRepos.bind(this);
@@ -25,12 +26,34 @@ class App extends React.Component {
     let members = await getMembers('emberjs');
     this.setState({ members, loading: false, repos: [] });
   }
+  handleSearchInputChange = (event) => {
+    this.setState({
+      searchValue: event.target.value
+    });
+  }
+  handleSearch = async (event) => {
+    event.preventDefault();
+    console.log(this.state.searchValue);
+    this.setState({ loading: true });
+
+    let [members, repos] = await Promise.all([
+      getMembers(this.state.searchValue),
+      getRepos(this.state.searchValue)
+    ]);
+
+    this.setState({ members, repos, loading: false });
+  }
   render() {
     return (
       <div>
+        <form onSubmit={this.handleSearch}>
+          <input
+            type="text"
+            value={this.state.searchValue}
+            onChange={this.handleSearchInputChange} />
+          <button type="submit">Search</button>
+        </form>
         {this.state.loading && <Loading />}
-        <button onClick={this.handleClickReposButton}>Repos</button>
-        <button onClick={this.fetchMembers}>Members</button>
         <div>
           <GitHubReposList repositories={this.state.repos} />
           <GitHubMembersList members={this.state.members} />
